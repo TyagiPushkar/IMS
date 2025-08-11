f87bf8e5be78dc54da155668bf584e7e55ee6fe3
yashxjain <jyash5332@gmail.com>
2025-08-11 12:06:06 +0530

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
   },
-  plugins: [
-    // ...
-    require("@tailwindcss/forms"),
-  ],
-};
+}
+
