611875153939be0e36b8c25c78bc66b54f3f746b
yashxjain <jyash5332@gmail.com>
2025-08-13 14:15:24 +0530

    new

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -108,7 +108,7 @@ export default function Login() {
                 id="AdminMail"
                 name="AdminMail"
                 type="email"
-                autocomplete="off"
+                autoComplete="email"
                 required
                 className="relative block w-full rounded-t-md border-0 py-1.5 px-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Email address"
@@ -124,7 +124,7 @@ export default function Login() {
   id="Password"
   name="Password"
   type="password"
-  autocomplete="off"  
+  autoComplete="new-password"  // Changed from "current-password"
   required
   className="relative block w-full border-0 py-1.5 px-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
   placeholder="Password"
